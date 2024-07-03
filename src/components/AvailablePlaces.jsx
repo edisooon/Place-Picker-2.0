019d9122c936cb72cb7ba15265941e221bb90e17
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-07-02 22:57:55 -0500

    implemented loading

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,13 +3,16 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsLoading(true);
       const resp = await fetch("http://localhost:3000/places");
       const jsonResp = await resp.json();
       setAvailablePlaces(jsonResp.places);
+      setIsLoading(false);
     }
     fetchPlaces();
   }, []);
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Loading places data now..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
