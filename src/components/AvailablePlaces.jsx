679488da8ae23fb6c16af2d570e5a54f281ed682
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-07-02 23:11:23 -0500

    implemented Error state

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isLoading, setIsLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsLoading(true);
-      const resp = await fetch("http://localhost:3000/places");
-      const jsonResp = await resp.json();
-      setAvailablePlaces(jsonResp.places);
+      try {
+        const resp = await fetch("http://localhost:3000/places");
+        const jsonResp = await resp.json();
+        if (!resp.ok) {
+          throw Error("Oops! Something went wrong...");
+        }
+        setAvailablePlaces(jsonResp.places);
+      } catch (error) {
+        setError({
+          message: error.message || "Oops! Something went wrong...",
+        });
+      }
       setIsLoading(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />;
+  }
 
   return (
     <Places
